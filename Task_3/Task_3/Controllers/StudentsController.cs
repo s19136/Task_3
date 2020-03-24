77c4e8c0b9e4341827e5b2e5c09371befda12f52
McMelloy <demetriuszbaranowski@gmail.com>
2020-03-24 19:56:33 +0100

    task 3

---
+++ Task_3/Task_3/Controllers/StudentsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task_3.DAL;
+using Task_3.Models;
+
+namespace Task_3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        /*[HttpGet]
+        public string GetStudents(string orderBy)
+        {
+            return $"Kowalski, Malewicz, sorting={orderBy}";
+
+        }*/
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 2000)}";
+            return Ok(student);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMethod(int id)
+        {
+            return Ok("Delete completed");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutMethod(int id)
+        {
+            return Ok("Put completed");
+        }
+    }
+}
